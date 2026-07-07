96d3cf611b60a35d5476dee71be92ab577146338
giovannylindor <giovannylindor56@gmail.com>
2026-07-06 20:26:36 -0400

    Created general mockup w/ username, post, caption, w/ buttons.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import Card from "./components/Card"
+import './App.css'
 
 export default function App() {
   
 
   return (
     <>
+    <div className="parent">
       <Card /> 
+    </div>
     </>
   )
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -17,18 +17,62 @@ export default function Card(){
 
     return(
         <>
-            <div className="m-auto w-[500px]">
+            <div className="m-auto w-125">
 
+
+                {/* Top Section*/}
                 <div className="flex justify-between items-center">
-                <div className="flex justify-center items-center gap-[0.5rem]">
-                    <img src={info.pfp} alt={info.username} className="w-[50px] rounded-full"/>
-                    <h1 className="font-semibold">{info.username}</h1>
-                    <p className="text-sm">&bull; 1d</p>
+                    <div className="flex justify-center items-center gap-[0.5rem]">
+                        <img src={info.pfp} alt={info.username} className="w-[50px] rounded-full"/>
+                        <h1 className="font-semibold">{info.username}</h1>
+                        <p className="text-sm">&bull; 1d</p>
+                    </div>
+
+                    <div>
+                        <button className="p-[0.3rem] font-semibold bg-neutral-300 rounded-md text-sm w-[70px] cursor-pointer">Follow</button>
+                        {/* Ellipis coming soon*/}
+                    </div>
+               </div>
+
+
+
+               {/* Post Section*/}
+               <div>
+
+                {/* Image of Post*/}
+                <div>
+                    <img src={info.post} alt={info.username + "'s post"} 
+                    className="w-125 my-2"
+                    />
                 </div>
 
-                <div className="flex justify-center items-center">
-                    <button>Follow</button>
-                    {/* Ellipis coming soon*/}
+                {/* Icons */}
+                <div className="flex justify-between w-125">
+                    <div className="flex items-center gap-3">
+                        <img src="src/assets/heart.png" alt="like" 
+                        className="w-6.25 cursor-pointer"
+                        />
+
+                        <img src="/src/assets/chat.png" alt="comment" 
+                        className="w-5.5 cursor-pointer"
+                        />
+
+                        <img src="src/assets/send.png" alt="share" 
+                        className="w-6 cursor-pointer"
+                        />
+                    </div>
+
+                    <div>
+                        <img src="src/assets/save-instagram.png" alt="save" 
+                        className="w-6 cursor-pointer"
+                        />
+                    </div>
+                </div>
+
+                {/* Username & Caption */}
+                <div className="flex gap-2 mt-1">
+                    <h1 className="font-semibold">{info.username}</h1>
+                    <p>{info.caption}</p>
                 </div>
                </div>
 
